11cdde9c197d4e7697c7bd2f69bcb40a446687ec
Louis Pisani <louispisani@Louiss-MBP.home>
2020-06-18 19:44:10 -0400

    working on scroll, need to lower requests

--- public/content.js
+++ public/content.js
@@ -13,7 +13,7 @@ const rottenRequest = (title,divToChange) =>{
              .then((success) => success.json())
              .then((rating)=>{
                 console.log({rating});
-                divToChange.innerText = divToChange.innerText + ` IMDB: ${rating.Ratings[0]? rating.Ratings[0].Value: "n/a"}`+ `RT: ${rating.Ratings[1]?rating.Ratings[1].Value: "n/a"}`;
+                divToChange.innerText = divToChange.innerText + ` IMDB: ${rating.Ratings[0]? rating.Ratings[0].Value: "n/a"}`+ `| RT: ${rating.Ratings[1]?rating.Ratings[1].Value: "n/a"}`;
             })
         })
         .catch((error)=>{ console.log(error)});
@@ -21,13 +21,46 @@ const rottenRequest = (title,divToChange) =>{
 }
 
 
-const getFilmTitle = () =>{
-    let titles = document.getElementsByClassName("site-font-primary-color truncate");
+const getFilmTitle = (titles = document.getElementsByClassName("site-font-primary-color truncate")) =>{
+
     for(let title in titles){
-      let rating = rottenRequest(titles[title].innerText, titles[title]);
-    //   console.log({rating});
+        console.log(titles[title]);
+        if(titles[title].querySelector('ext-title') == null){
+            console.log("test");
+            let newDiv = document.createElement("div");
+            newDiv.className = "ext-title";
+            titles[title].appendChild(newDiv);
+            let rating = rottenRequest(titles[title].innerText, newDiv);
+        }
+    //   consChildole.log({rating});
     //   titles[title].innerHTML = titles[title].innerHTML + rating;
     }
 }
 
+const targetNode = document.getElementsByClassName('browse-rows')[0];
+const config = {  attributes: false, childList: true, subtree: false };
+
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    console.log("in callback")
+
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            console.log("in callback2")
+            getFilmTitle();
+            console.log('A child node has been added or removed.');
+        }
+        else if (mutation.type === 'subtree') {
+            console.log("in callback3")
+
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
 getFilmTitle();
+
+
